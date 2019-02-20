c203b38f27e649c36d7f248e0067cb712b41e47e
Divya Kali Ranga Fnu <dkr.fnu@criteo.com>
2019-02-19 21:01:47 -0800

    Refactor post-submit and pre-submit scripts
    Jira: EE-124
    Change-Id: I4e6cfd5f217ecd5fea4e958ca6702de337b16e06

--- scripts/build-merged.sh
+++ scripts/build-merged.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -l
 
 echo "Building and publishing artifacts to nexus"
 ORIGINAL_VERSION=2.0.0
 VERSION=$ORIGINAL_VERSION-criteo-$(date -u +%Y%m%d%H%M%S)
-./gradlew clean build publish -Pversion=$VERSION -PpublishUrl="http://nexus.criteo.prod/content/repositories/criteo.android.releases/"
+sh scripts/build-pre-submit.sh
+sh gradlew publish -Pversion=$VERSION -PpublishUrl="http://nexus.criteo.prod/content/repositories/criteo.android.releases/"
--- scripts/build-pre-submit.sh
+++ scripts/build-pre-submit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 echo "Running a clean build on the mochi project for presubmit"
 ./gradlew clean build --info --stacktrace
