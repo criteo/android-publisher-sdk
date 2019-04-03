9a76748d3c09e22af3c9550e0db8d75775bfed76
Divya Kali Ranga Fnu <dkr.fnu@criteo.com>
2019-04-03 14:09:17 -0700

    Some refactoring
    Change-Id: Idd7cd7391399b921e4e97e9fc33abb332a0a99f1
    JIRA: EE-147

--- mediation/src/main/java/com/criteo/mediation/adapter/CriteoAdapter.java
+++ mediation/src/main/java/com/criteo/mediation/adapter/CriteoAdapter.java
@@ -4,10 +4,9 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
-
+import com.criteo.publisher.Criteo;
 import com.criteo.publisher.mediation.controller.CriteoBannerEventController;
 import com.criteo.publisher.mediation.controller.CriteoInterstitialEventController;
-import com.criteo.publisher.Criteo;
 import com.criteo.publisher.model.AdUnit;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
@@ -17,12 +16,10 @@ import com.google.android.gms.ads.mediation.customevent.CustomEventBannerListene
 import com.google.android.gms.ads.mediation.customevent.CustomEventInterstitial;
 import com.google.android.gms.ads.mediation.customevent.CustomEventInterstitialListener;
 import com.google.android.gms.ads.mediation.customevent.CustomEventListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial {
 
@@ -44,14 +41,11 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
      */
     @Override
     public void onDestroy() {
-//        if (bannerEventController != null) {
-//            bannerEventController.criteoBannerView.destroy();
-//        }
     }
 
     /**
-     * The app is being paused. This call will only be forwarded to the adapter if the developer
-     * notifies mediation that the app is being paused.
+     * The app is being paused. This call will only be forwarded to the adapter if the developer notifies mediation that
+     * the app is being paused.
      */
     @Override
     public void onPause() {
@@ -59,8 +53,8 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
     }
 
     /**
-     * The app is being resumed. This call will only be forwarded to the adapter if the developer
-     * notifies mediation that the app is being resumed.
+     * The app is being resumed. This call will only be forwarded to the adapter if the developer notifies mediation
+     * that the app is being resumed.
      */
     @Override
     public void onResume() {
@@ -69,11 +63,11 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
 
     @Override
     public void requestBannerAd(Context context,
-                                CustomEventBannerListener listener,
-                                String serverParameter,
-                                AdSize size,
-                                MediationAdRequest mediationAdRequest,
-                                Bundle customEventExtras) {
+            CustomEventBannerListener listener,
+            String serverParameter,
+            AdSize size,
+            MediationAdRequest mediationAdRequest,
+            Bundle customEventExtras) {
         /*
          * 1. Initialize Criteo
          * 2. Create your banner view.
@@ -92,19 +86,20 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
 
         // Initialize Criteo
         if (serverParameter != null) {
-            initializeCriteo(serverParameter, context, listener);
-        } else
+            initialize(serverParameter, context, listener);
+        } else {
             listener.onAdFailedToLoad(AdRequest.ERROR_CODE_INTERNAL_ERROR);
+        }
 
 
     }
 
     @Override
     public void requestInterstitialAd(Context context,
-                                      CustomEventInterstitialListener listener,
-                                      String serverParameter,
-                                      MediationAdRequest mediationAdRequest,
-                                      Bundle customEventExtras) {
+            CustomEventInterstitialListener listener,
+            String serverParameter,
+            MediationAdRequest mediationAdRequest,
+            Bundle customEventExtras) {
         /*
          * In this method, you should:
          *
@@ -123,12 +118,13 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
 
         // Initialize Criteo
         if (serverParameter != null) {
-            initializeCriteo(serverParameter, context, listener);
-        } else
+            initialize(serverParameter, context, listener);
+        } else {
             listener.onAdFailedToLoad(AdRequest.ERROR_CODE_INTERNAL_ERROR);
+        }
     }
 
-    private void initializeCriteo(String serverParameter, Context context, CustomEventListener listener) {
+    private void initialize(String serverParameter, Context context, CustomEventListener listener) {
         int networkId = 0;
         String placementId = "";
         int width = 0;
@@ -137,8 +133,9 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
         if (!TextUtils.isEmpty(serverParameter)) {
             try {
                 parameters = new JSONObject(serverParameter);
-                if (parameters.get(NETWORKID) != null)
+                if (parameters.get(NETWORKID) != null) {
                     networkId = parameters.getInt(NETWORKID);
+                }
                 if (parameters.get(ADUNIT) != null) {
                     JSONObject adUnit = parameters.getJSONObject(ADUNIT);
                     placementId = adUnit.getString(PLACEMENTID);
@@ -146,13 +143,10 @@ public class CriteoAdapter implements CustomEventBanner, CustomEventInterstitial
                     height = adUnit.getInt(HEIGHT);
                 }
 
-                this.adUnits = new ArrayList<>();
-                AdUnit adUnit = new AdUnit();
-                adUnit.setPlacementId(placementId);
-                com.criteo.publisher.model.AdSize adSize = new com.criteo.publisher.model.AdSize();
-                adSize.setWidth(width);
-                adSize.setHeight(height);
-                adUnit.setSize(adSize);
+                com.criteo.publisher.model.AdSize adSize = new com.criteo.publisher.model.AdSize(width, height);
+                AdUnit adUnit = new AdUnit(adSize, placementId);
+
+                adUnits = new ArrayList<>();
                 adUnits.add(adUnit);
 
                 Criteo.init((Application) (context.getApplicationContext()), adUnits, networkId);
