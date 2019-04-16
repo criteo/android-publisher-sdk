9127daa2aae2384aa972cc8b5f42a09354d809b7
Husnu Uzulmez <h.uzulmez@criteo.com>
2019-04-16 13:40:10 -0700

    Added asynchronous task for banner
    Change-Id: I0278dce17c407ebbb5a809df853a7b38da53fd71
    JIRA: EE-179

--- publisher-sdk/src/main/java/com/criteo/publisher/mediation/tasks/CriteoBannerFetchTask.java
+++ publisher-sdk/src/main/java/com/criteo/publisher/mediation/tasks/CriteoBannerFetchTask.java
@@ -1,31 +1,38 @@
 package com.criteo.publisher.mediation.tasks;
 
-import android.content.Context;
 import android.os.AsyncTask;
-
 import com.criteo.publisher.mediation.listeners.CriteoBannerAdListener;
+import com.criteo.publisher.mediation.utils.CriteoErrorCode;
 import com.criteo.publisher.mediation.view.CriteoBannerView;
+import com.criteo.publisher.model.Slot;
 
 
-public class CriteoBannerFetchTask extends AsyncTask<Void, Void, Void> {
+public class CriteoBannerFetchTask extends AsyncTask<Slot, Void, Slot> {
 
     private CriteoBannerView criteoBannerView;
-    private Context context;
     private CriteoBannerAdListener criteoBannerAdListener;
 
-    public CriteoBannerFetchTask(Context context, CriteoBannerView bannerView, CriteoBannerAdListener listener) {
+    public CriteoBannerFetchTask(CriteoBannerView bannerView, CriteoBannerAdListener listener) {
         this.criteoBannerView = bannerView;
-        this.context = context;
         this.criteoBannerAdListener = listener;
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        return null;
+    protected Slot doInBackground(Slot... slots) {
+        if (slots == null) {
+            return null;
+        }
+        Slot slot = slots[0];
+        return slot;
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(Slot slot) {
+        super.onPostExecute(slot);
+        if (slot == null) {
+            criteoBannerAdListener.onAdFetchFailed(CriteoErrorCode.ERROR_CODE_NO_FILL);
+        } else {
+            criteoBannerAdListener.onAdFetchSucceededForBanner(criteoBannerView);
+        }
     }
 }
--- publisher-sdk/src/main/java/com/criteo/publisher/mediation/view/CriteoBannerView.java
+++ publisher-sdk/src/main/java/com/criteo/publisher/mediation/view/CriteoBannerView.java
@@ -2,11 +2,10 @@ package com.criteo.publisher.mediation.view;
 
 import android.content.Context;
 import android.webkit.WebView;
-
-import com.criteo.publisher.mediation.listeners.CriteoBannerAdListener;
 import com.criteo.publisher.mediation.controller.CriteoBannerEventController;
+import com.criteo.publisher.mediation.listeners.CriteoBannerAdListener;
+import com.criteo.publisher.model.Slot;
 import com.google.android.gms.ads.AdSize;
-
 import java.lang.ref.WeakReference;
 
 public class CriteoBannerView extends WebView {
@@ -14,6 +13,8 @@ public class CriteoBannerView extends WebView {
     private AdSize adSize;
 
     private String adUnit;
+
+    private Slot slot;
 
     private CriteoBannerAdListener criteoBannerAdListener;
 
@@ -28,6 +29,9 @@ public class CriteoBannerView extends WebView {
         this.criteoBannerAdListener = criteoBannerAdListener;
     }
 
+    public Slot getSlot() {
+        return slot;
+    }
 
     public void setAdSize(AdSize adSize) {
         this.adSize = adSize;
@@ -45,7 +49,7 @@ public class CriteoBannerView extends WebView {
         return adUnit;
     }
 
-    void loadAd() {
+    public void loadAd() {
 
     }
 }
