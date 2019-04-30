f0886c1fd00bdaee725968c788bed2d0bf9cc534
Husnu Uzulmez <h.uzulmez@criteo.com>
2019-04-30 15:18:19 -0700

    Added view controller for Interstitial
    Change-Id: Ibc0603d3092418196b6d087fc4937e8809d7c140
    JIRA: EE-187

--- publisher-sdk/src/main/java/com/criteo/publisher/mediation/controller/CriteoInterstitialEventController.java
+++ publisher-sdk/src/main/java/com/criteo/publisher/mediation/controller/CriteoInterstitialEventController.java
@@ -1,14 +1,21 @@
 package com.criteo.publisher.mediation.controller;
 
 import android.content.Context;
+import com.criteo.publisher.Criteo;
 import com.criteo.publisher.mediation.listeners.CriteoInterstitialAdListener;
 import com.criteo.publisher.mediation.tasks.CriteoInterstitialFetchTask;
 import com.criteo.publisher.mediation.view.CriteoInterstitialView;
+import com.criteo.publisher.model.AdUnit;
+import com.criteo.publisher.model.Config;
+import com.criteo.publisher.model.Slot;
 
 
 public class CriteoInterstitialEventController {
 
+    private static final String DISPLAY_URL_MACRO = "%%displayUrl%%";
+
     private Context context;
+    private boolean loaded;
     private CriteoInterstitialView criteoInterstitialview;
     private CriteoInterstitialAdListener criteoInterstitialAdListener;
     private CriteoInterstitialFetchTask criteoInterstitialFetchTask;
@@ -18,17 +25,35 @@ public class CriteoInterstitialEventController {
         this.criteoInterstitialview = interstitialview;
         this.criteoInterstitialAdListener = listener;
         this.context = context;
+        this.loaded = false;
     }
 
     private boolean isAdLoaded() {
-        return true;
+        return loaded;
     }
 
-    void fetchAdAsync() {
+    void fetchAdAsync(AdUnit adUnit) {
+        loaded = false;
+        Slot slot = Criteo.getInstance().getBidForAdUnit(adUnit);
 
+        criteoInterstitialFetchTask = new CriteoInterstitialFetchTask(criteoInterstitialAdListener);
+        criteoInterstitialFetchTask.execute(slot);
+
+        if (slot != null) {
+            String displayUrlWithTag = Config.mediationAdTag;
+            String displayUrl = displayUrlWithTag.replace(DISPLAY_URL_MACRO, slot.getDisplayUrl());
+
+            //gets Webview data from Criteo before showing Interstitialview Activity
+            getWebviewDataAsync(displayUrl);
+        }
     }
 
     public void show() {
 
     }
+
+    private void getWebviewDataAsync(String displayUrl) {
+
+    }
+
 }
