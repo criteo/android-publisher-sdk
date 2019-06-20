1a3b8c7e6e84aa90bab2cfd20549183de29f8d8d
Husnu Uzulmez <h.uzulmez@criteo.com>
2019-06-20 14:19:24 -0700

    In-house mediation Added Interstitial into the  sample activity
    Change-Id: I12281b743c9cc52d3576def64822e7d76df22b59
    JIRA: EE-263

--- app/src/main/java/com/criteo/pubsdk_android/InHouseActivity.java
+++ app/src/main/java/com/criteo/pubsdk_android/InHouseActivity.java
@@ -6,14 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import com.criteo.publisher.Criteo;
 import com.criteo.publisher.Util.CriteoErrorCode;
 import com.criteo.publisher.listener.CriteoBannerAdListener;
+import com.criteo.publisher.listener.CriteoInterstitialAdListener;
 import com.criteo.publisher.mediation.view.CriteoBannerView;
+import com.criteo.publisher.mediation.view.CriteoInterstitialView;
 import com.criteo.publisher.model.AdSize;
 import com.criteo.publisher.model.BannerAdUnit;
 import com.criteo.publisher.model.BidResponse;
+import com.criteo.publisher.model.InterstitialAdUnit;
 
 public class InHouseActivity extends AppCompatActivity {
 
@@ -23,6 +27,9 @@ public class InHouseActivity extends AppCompatActivity {
     private LinearLayout adLayout;
     private CriteoBannerAdListener criteoBannerAdListener;
     private CriteoBannerView criteoBannerView;
+    private CriteoInterstitialView criteoInterstitialView;
+    private Button buttonInhouseInterstitial;
+    private CriteoInterstitialAdListener criteoInterstitialAdListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +37,12 @@ public class InHouseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_in_house);
 
         adLayout = findViewById(R.id.AdLayout);
+        buttonInhouseInterstitial = findViewById(R.id.buttonInhouseInterstitial);
         context = getApplicationContext();
 
         createAdListener();
 
-        findViewById(R.id.buttonStandAloneBanner).setOnClickListener(new OnClickListener() {
+        findViewById(R.id.buttonInhouseBanner).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 adLayout.removeAllViews();
@@ -51,7 +59,31 @@ public class InHouseActivity extends AppCompatActivity {
             }
         });
 
+        buttonInhouseInterstitial.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (criteoInterstitialView.isAdLoaded()) {
+                    criteoInterstitialView.show();
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        buttonInhouseInterstitial.setEnabled(false);
+        interstitialAdLoad();
+    }
+
+    private void interstitialAdLoad() {
+        InterstitialAdUnit interstitialAdUnit = new InterstitialAdUnit("/140800857/Endeavour_Interstitial_320x480");
+        criteoInterstitialView = new CriteoInterstitialView(context, interstitialAdUnit);
+        criteoInterstitialView.setCriteoInterstitialAdListener(criteoInterstitialAdListener);
+        criteoInterstitialView.loadAd();
+    }
+
 
     private void createAdListener() {
         criteoBannerAdListener = new CriteoBannerAdListener() {
@@ -82,6 +114,33 @@ public class InHouseActivity extends AppCompatActivity {
             }
         };
 
+        criteoInterstitialAdListener = new CriteoInterstitialAdListener() {
+            @Override
+            public void onAdFetchSucceeded() {
+                buttonInhouseInterstitial.setEnabled(true);
+                Log.d(TAG, "Interstitial ad loaded");
+            }
+
+            @Override
+            public void onAdFetchFailed(CriteoErrorCode code) {
+                Log.d(TAG, "Interstitial ad failed");
+            }
+
+            @Override
+            public void onAdClicked() {
+                Log.d(TAG, "Interstitial ad clicked");
+            }
+
+            @Override
+            public void onAdFullScreen() {
+                Log.d(TAG, "Interstitial ad full screen");
+            }
+
+            @Override
+            public void onAdClosed() {
+                Log.d(TAG, "Interstitial ad closed");
+            }
+        };
     }
 
 }
