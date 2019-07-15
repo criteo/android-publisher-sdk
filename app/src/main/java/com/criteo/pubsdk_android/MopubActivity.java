47a235a6243f73cbdc2fbad02399ae11af627988
Husnu Uzulmez <h.uzulmez@criteo.com>
2019-07-15 14:25:21 -0700

    Test MoPub Integration
    Change-Id: If8d98d0de84b8242977e2b6d411a50deecb39635
    JIRA: EE-348

--- app/src/main/java/com/criteo/pubsdk_android/MopubActivity.java
+++ app/src/main/java/com/criteo/pubsdk_android/MopubActivity.java
@@ -1,5 +1,7 @@
 package com.criteo.pubsdk_android;
 
+import static com.criteo.pubsdk_android.PubSdkDemoApplication.MOPUB_BANNER_ADUNIT_ID;
+import static com.criteo.pubsdk_android.PubSdkDemoApplication.MOPUB_INTERSTITIAL_ADUNIT_ID;
 import static com.mopub.common.logging.MoPubLog.LogLevel.DEBUG;
 import static com.mopub.common.logging.MoPubLog.LogLevel.INFO;
 
@@ -10,32 +12,35 @@ import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import com.criteo.publisher.Criteo;
-import com.criteo.publisher.model.BannerAdUnit;
 import com.criteo.publisher.model.AdSize;
+import com.criteo.publisher.model.BannerAdUnit;
+import com.criteo.publisher.model.InterstitialAdUnit;
 import com.mopub.common.MoPub;
 import com.mopub.common.SdkConfiguration;
 import com.mopub.common.SdkInitializationListener;
+import com.mopub.mobileads.MoPubErrorCode;
+import com.mopub.mobileads.MoPubInterstitial;
+import com.mopub.mobileads.MoPubInterstitial.InterstitialAdListener;
 import com.mopub.mobileads.MoPubView;
 
 public class MopubActivity extends AppCompatActivity {
 
+    private static final String TAG = MopubActivity.class.getSimpleName();
+    private static final String SDK_BUILD_ID = "b195f8dd8ded45fe847ad89ed1d016da";
     private MoPubView publisherAdView;
+    private MoPubInterstitial mInterstitial;
     private LinearLayout linearLayout;
-    private static final String AD_UNIT = "b5acf501d2354859941b13030d2d848a";
-    private static final String CRT_DISPLAY_URL = "crt_displayUrl";
-    private static final String CRT_DISPLAY_CREATIVE = "https://rdi.us.criteo.com/delivery/r/ajs.php?did=5c9d31c92edc9dc67203ee38b6f43a00&u=%7CgfenuDf6GAt0T4ax9G2074VyZUuINvEBORTJkP12BIo%3D%7C&c1=fYGSyyN4O4mkT2ynhzfwbdpiG7v0SMGpyk_8mUbwNIml5THlXxSIQO_JQSnOsUNK2v58ibue4bk7EcIIU1dEF0RbfDQ4KzirymhRPnuZLWBkvVfFezrMwdYTaTH23G4-HEgtuf6PuQgOSCpErOm8LzlaTECFqL4xfFsuAg-1I0K5XZMzGMzWs6aJLWRK0XergQrbg95DEUOLZ37n5AAwFZP3TpzfL3D97JU8jfx2Vp6rs70dolZJHXTd-uUeQJwNBgb2TEnItXx7a5vqk7ssFiPjoVX3NOcJVeMXw5wLYXyJw06-odGbGqfAoMIAkXvTmM1cw-lFf8BYgWoRzd7CTiVDtjo69dZ5mIOme7yDxh2dDHrGNYi6KYgl6Zt8UMOlnSZ4Be4mngs1k8RzVTFZPX78UP3uPUa4i7d5xG3kKXd8n6Fcfwu1_EJBilXjg7uV-4AJT62UFTQ";
     private Criteo criteo;
+    private InterstitialAdListener interstitialAdListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mopub);
-
-        //MoPub.setLocationAwareness(MoPub.LocationAwareness.TRUNCATED);
-        //MoPub.setLocationPrecision(4);
         criteo = Criteo.getInstance();
+        createInterstitialAdListener();
 
-        final SdkConfiguration.Builder configBuilder = new SdkConfiguration.Builder("b195f8dd8ded45fe847ad89ed1d016da");
+        final SdkConfiguration.Builder configBuilder = new SdkConfiguration.Builder(SDK_BUILD_ID);
         if (BuildConfig.DEBUG) {
             configBuilder.withLogLevel(DEBUG);
         } else {
@@ -44,7 +49,7 @@ public class MopubActivity extends AppCompatActivity {
         MoPub.initializeSdk(this, configBuilder.build(), new SdkInitializationListener() {
             @Override
             public void onInitializationFinished() {
-                Log.d("test", "Succss");
+                Log.d(TAG, "Mopub initialization completed");
             }
         });
 
@@ -53,26 +58,71 @@ public class MopubActivity extends AppCompatActivity {
             onBannerClick();
         });
 
+        findViewById(R.id.buttonInterstitial).setOnClickListener((View v) -> {
+            onInterstitialClick();
+        });
+    }
+
+    private void createInterstitialAdListener() {
+        interstitialAdListener = new InterstitialAdListener() {
+            @Override
+            public void onInterstitialLoaded(MoPubInterstitial interstitial) {
+                Log.d(TAG, "Mopub ad loaded");
+                mInterstitial.show();
+
+            }
+
+            @Override
+            public void onInterstitialFailed(MoPubInterstitial interstitial, MoPubErrorCode errorCode) {
+                // Code to be executed when an ad request fails
+                Log.d(TAG, "Mopub ad failed:" + errorCode);
+            }
+
+            @Override
+            public void onInterstitialShown(MoPubInterstitial interstitial) {
+                Log.d("TAG", "ad shown");
+            }
+
+            @Override
+            public void onInterstitialClicked(MoPubInterstitial interstitial) {
+                Log.d(TAG, "Mopub ad clicked");
+
+            }
+
+            @Override
+            public void onInterstitialDismissed(MoPubInterstitial interstitial) {
+                Log.d(TAG, "Mopub ad dismissed");
+
+            }
+        };
     }
 
     private void onBannerClick() {
-        BannerAdUnit moPub = new BannerAdUnit("b5acf501d2354859941b13030d2d848a", new AdSize(320, 50));
+        BannerAdUnit moPub = new BannerAdUnit(MOPUB_BANNER_ADUNIT_ID, new AdSize(320, 50));
         linearLayout.setBackgroundColor(Color.RED);
         linearLayout.removeAllViews();
         linearLayout.setVisibility(View.VISIBLE);
         publisherAdView = new MoPubView(this);
-        publisherAdView.setKeywords("key1:value1,key2:value2");
         criteo.setBidsForAdUnit(publisherAdView, moPub);
-        publisherAdView.setAdUnitId(AD_UNIT);
-
-        //PublisherAdRequest request = Criteo.getInstance().setBidsForAdUnit(builder, adUnit).build();
+        publisherAdView.setAdUnitId(MOPUB_BANNER_ADUNIT_ID);
         publisherAdView.loadAd();
         linearLayout.addView(publisherAdView);
+    }
+
+    private void onInterstitialClick() {
+        InterstitialAdUnit moPubInterstitialAdUnit = new InterstitialAdUnit(
+                MOPUB_INTERSTITIAL_ADUNIT_ID);
+        mInterstitial = new MoPubInterstitial(this, MOPUB_INTERSTITIAL_ADUNIT_ID);
+        criteo.setBidsForAdUnit(mInterstitial, moPubInterstitialAdUnit);
+        mInterstitial.setInterstitialAdListener(interstitialAdListener);
+        mInterstitial.load();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        publisherAdView.destroy();
+        if (publisherAdView != null) {
+            publisherAdView.destroy();
+        }
     }
 }
--- app/src/main/java/com/criteo/pubsdk_android/PubSdkDemoApplication.java
+++ app/src/main/java/com/criteo/pubsdk_android/PubSdkDemoApplication.java
@@ -1,19 +1,24 @@
 package com.criteo.pubsdk_android;
 
 import android.app.Application;
-import android.net.TrafficStats;
 import android.os.StrictMode;
 import android.util.Log;
 import com.criteo.publisher.Criteo;
-import com.criteo.publisher.model.BannerAdUnit;
-import com.criteo.publisher.model.InterstitialAdUnit;
 import com.criteo.publisher.model.AdSize;
 import com.criteo.publisher.model.AdUnit;
+import com.criteo.publisher.model.BannerAdUnit;
+import com.criteo.publisher.model.InterstitialAdUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PubSdkDemoApplication extends Application {
+
     private static final String TAG = PubSdkDemoApplication.class.getSimpleName();
+    public static final String DFP_BANNER_ADUNIT_ID = "/140800857/Endeavour_320x50";
+    public static final String DFP_INTERSTITIAL_ADUNIT_ID = "/140800857/Endeavour_Interstitial_320x480";
+    public static final String MOPUB_BANNER_ADUNIT_ID = "b5acf501d2354859941b13030d2d848a";
+    public static final String MOPUB_INTERSTITIAL_ADUNIT_ID = "86c36b6223ce4730acf52323de3baa93";
+
 
     @Override
     public void onCreate() {
@@ -28,15 +33,18 @@ public class PubSdkDemoApplication extends Application {
 
         List<AdUnit> adUnits = new ArrayList<>();
 
-        BannerAdUnit bannerAdUnit = new BannerAdUnit("/140800857/Endeavour_320x50", new AdSize(320, 50));
+        BannerAdUnit bannerAdUnit = new BannerAdUnit(DFP_BANNER_ADUNIT_ID, new AdSize(320, 50));
         adUnits.add(bannerAdUnit);
 
-//        BannerAdUnit moPubAdUnit = new BannerAdUnit("b5acf501d2354859941b13030d2d848a");
-//        bannerAdUnit.setAdSize(new AdSize(320, 50));
-//        adUnits.add(moPubAdUnit);
-
-        InterstitialAdUnit interstitialAdUnit = new InterstitialAdUnit("/140800857/Endeavour_Interstitial_320x480");
+        InterstitialAdUnit interstitialAdUnit = new InterstitialAdUnit(DFP_INTERSTITIAL_ADUNIT_ID);
         adUnits.add(interstitialAdUnit);
+
+        BannerAdUnit moPubBannerAdUnit = new BannerAdUnit(MOPUB_BANNER_ADUNIT_ID, new AdSize(320, 50));
+        adUnits.add(moPubBannerAdUnit);
+
+        InterstitialAdUnit moPubInterstitialAdUnit = new InterstitialAdUnit(MOPUB_INTERSTITIAL_ADUNIT_ID);
+        adUnits.add(moPubInterstitialAdUnit);
+
 
         try {
             Criteo.init(this, "B-056946", adUnits);
