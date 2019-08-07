328daf5b8c39ea38474eab49bbd29a9100bad855
Adwait Kulkarni <a.kulkarni@criteo.com>
2019-08-07 10:59:56 -0700

    Compile Android tests in build scripts
    Android tests aren't compiled in the build script, this causes issues when code changes
    are made and the android tests haven't been updated/compiled/run.
    
    While this will not run the android tests on Jenkins it will at least provide
    feedback to the author and the reviewers that something is breaking and
    probably needs looking into.
    
    Change-Id: I4f2e95eecc306d87080c71364f32987884177367

--- scripts/build-pre-submit.sh
+++ scripts/build-pre-submit.sh
@@ -3,4 +3,4 @@
 echo "Setting up enviorenment variables to log.."
 export CRITEO_LOGGING=true
 echo "Running a clean build on the mochi project for presubmit"
-./gradlew clean build --info --stacktrace
+./gradlew clean build assembleAndroidTest --info --stacktrace
