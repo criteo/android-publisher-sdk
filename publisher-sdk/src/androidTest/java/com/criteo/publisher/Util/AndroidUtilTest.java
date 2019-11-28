e6fe59cee2329c1b9314adbd3a9c186dfcd47ef0
p.turpin <p.turpin@criteo.com>
2019-11-28 10:07:11 +0100

    Test detection of the screen orientation
    A dummy activity needs to be created on debug production code side.
    Putting it in the test side does not seems to work correctly (see
    https://stackoverflow.com/a/51584793 for more information).
    This is a safe solution as we should only export release to publishers.
    Solving EE-598 will yield a proper solution as the test activities could
    then live in a dedicated test module outside of the sdk one.
    
    Change-Id: I4fd9e6df30ad01849bc7efd13cb34435240cbcdf
    JIRA: EE-593

---
+++ publisher-sdk/src/androidTest/java/com/criteo/publisher/Util/AndroidUtilTest.java
@@ -0,0 +1,69 @@
+package com.criteo.publisher.Util;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.test.rule.ActivityTestRule;
+import com.criteo.publisher.test.activity.DummyActivity;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AndroidUtilTest {
+
+  @Rule
+  public ActivityTestRule<DummyActivity> activityRule = new ActivityTestRule<>(DummyActivity.class);
+
+  private AndroidUtil androidUtil;
+
+  @Before
+  public void setUp() throws Exception {
+    androidUtil = new AndroidUtil(activityRule.getActivity().getApplicationContext());
+  }
+
+  @Test
+  public void getOrientation_GivenDeviceInPortrait_ReturnPortrait() throws Exception {
+    activityRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    waitForOrientation();
+
+    int orientation = androidUtil.getOrientation();
+
+    assertEquals(Configuration.ORIENTATION_PORTRAIT, orientation);
+  }
+
+  @Test
+  public void getOrientation_GivenDeviceInReversePortrait_ReturnPortrait() throws Exception {
+    activityRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+    waitForOrientation();
+
+    int orientation = androidUtil.getOrientation();
+
+    assertEquals(Configuration.ORIENTATION_PORTRAIT, orientation);
+  }
+
+  @Test
+  public void getOrientation_GivenDeviceInLandscape_ReturnLandscape() throws Exception {
+    activityRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    waitForOrientation();
+
+    int orientation = androidUtil.getOrientation();
+
+    assertEquals(Configuration.ORIENTATION_LANDSCAPE, orientation);
+  }
+
+  @Test
+  public void getOrientation_GivenDeviceInReverseLandscape_ReturnLandscape() throws Exception {
+    activityRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+    waitForOrientation();
+
+    int orientation = androidUtil.getOrientation();
+
+    assertEquals(Configuration.ORIENTATION_LANDSCAPE, orientation);
+  }
+
+  private void waitForOrientation() throws InterruptedException {
+    Thread.sleep(500); // FIXME EE-657
+  }
+
+}
---
+++ publisher-sdk/src/debug/java/com/criteo/publisher/test/activity/DummyActivity.java
@@ -0,0 +1,7 @@
+package com.criteo.publisher.test.activity;
+
+import android.app.Activity;
+
+public class DummyActivity extends Activity {
+
+}
