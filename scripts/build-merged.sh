8824b184c8a68f2c5c3db1bc7c753893e8b62b87
p.turpin <p.turpin@criteo.com>
2020-02-24 11:25:22 +0100

    Execute post-submit in docker container
    Change-Id: Id51c6ee43e6475381a3659bc3eb24932c9983295
    JIRA: EE-654

--- scripts/build-merged.sh
+++ scripts/build-merged.sh
@@ -1,9 +1,38 @@
 #!/bin/bash -l
 
-echo "Building and publishing artifacts to nexus"
-ORIGINAL_VERSION="3.4.0"
-VERSION="$ORIGINAL_VERSION-criteo-$(date -u +%Y%m%d%H%M%S)"
-NEXUS_URL="http://nexus.criteo.prod/content/repositories/criteo.android.releases/"
+# Run this script to assemble and publish artifacts to Criteo nexus.
+# You do not need to set up any environment, as a docker container will execute it.
 
-./scripts/build-pre-submit.sh
-./gradlew publish -Pversion="$VERSION" -PpublishUrl="$NEXUS_URL"
+# Explicitly bound environment variables
+MAVEN_USER="${MAVEN_USER}"
+MAVEN_PASSWORD="${MAVEN_PASSWORD}"
+
+set -Eeuo pipefail
+
+# Go at the root of the mochi directory
+cd "$(dirname "$0")/.."
+
+SCRIPT="./scripts/do-post-submit.sh"
+DOCKER_IMAGE="criteo-publishersdk-android"
+SRC="$(pwd)"
+DST="/mochi"
+
+# Workaround for Windows. See: https://github.com/docker/toolbox/issues/673
+export MSYS_NO_PATHCONV=1
+
+echo "Building docker image"
+docker build \
+    --build-arg UID="$(id -u)" \
+    --build-arg GID="$(id -g)" \
+    -t "${DOCKER_IMAGE}" \
+    .
+
+echo "Running post-submit in docker container"
+docker run \
+    --rm \
+    -v "${SRC}:${DST}" \
+    -w "${DST}" \
+    -e "MAVEN_USER=${MAVEN_USER}" \
+    -e "MAVEN_PASSWORD=${MAVEN_PASSWORD}" \
+    ${DOCKER_IMAGE} \
+    bash "$SCRIPT"
---
+++ scripts/do-post-submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+# Run this script to assemble and publish artifacts to Criteo nexus.
+# If you run this script manually, make sure to have the Android tools available. Else, you may
+# instead run the ./scripts/build-merged.sh script, to wrap the build in a docker container with
+# sufficient tooling.
+
+set -Eeuo pipefail
+
+echo "Building and publishing artifacts to nexus"
+ORIGINAL_VERSION="3.4.0"
+VERSION="$ORIGINAL_VERSION-criteo-$(date -u +%Y%m%d%H%M%S)"
+NEXUS_URL="http://nexus.criteo.prod/content/repositories/criteo.android.releases/"
+
+./scripts/build-pre-submit.sh
+./gradlew publish -Pversion="$VERSION" -PpublishUrl="$NEXUS_URL"
