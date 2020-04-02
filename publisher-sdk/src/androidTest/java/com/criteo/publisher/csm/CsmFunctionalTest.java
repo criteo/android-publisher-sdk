bd97397b24aec90665319dd65714a925e6ae0926
p.turpin <p.turpin@criteo.com>
2020-04-02 15:25:06 +0200

    Validate that CSM request is sent when network error happens
    Tested scenario:
    - Given three ad units A, B and C
    - Given SDK prefetching both A and B
    - Given network error while calling CDB for bid
    - When asking for bid on C
    - Then metric request is sent to CDB CSM endpoint
    - Then metric request is for both A and B
    - Then metric request shows that A was a network error
    - Then metric request shows that B was a network error
    
    Change-Id: I7b454715ce777f0d2c1714162b1ac822be259802
    JIRA: EE-951

--- publisher-sdk/src/androidTest/java/com/criteo/publisher/csm/CsmFunctionalTest.java
+++ publisher-sdk/src/androidTest/java/com/criteo/publisher/csm/CsmFunctionalTest.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.clearInvocations;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,6 +23,7 @@ import com.criteo.publisher.csm.MetricRequest.MetricRequestFeedback;
 import com.criteo.publisher.mock.MockedDependenciesRule;
 import com.criteo.publisher.mock.SpyBean;
 import com.criteo.publisher.network.PubSdkApi;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.inject.Inject;
 import org.junit.After;
@@ -130,6 +133,34 @@ public class CsmFunctionalTest {
     }));
   }
 
+  @Test
+  public void givenNetworkErrorFromCdb_CallApiWithCsmOfNetworkError() throws Exception {
+    doThrow(IOException.class).when(api).loadCdb(any(), any());
+
+    CriteoUtil.givenInitializedCriteo(TestAdUnits.BANNER_320_50, TestAdUnits.INTERSTITIAL);
+    waitForIdleState();
+
+    Criteo.getInstance().getBidResponse(TestAdUnits.INTERSTITIAL_UNKNOWN);
+    waitForIdleState();
+
+    verify(api).postCsm(argThat(request -> {
+      assertRequestHeaderIsExpected(request);
+
+      assertEquals(2, request.getFeedbacks().size());
+      MetricRequestFeedback feedback1 = request.getFeedbacks().get(0);
+      MetricRequestFeedback feedback2 = request.getFeedbacks().get(1);
+      assertItRepresentsNetworkError(feedback1);
+      assertItRepresentsNetworkError(feedback2);
+
+      assertNotEquals(
+          feedback1.getSlots().get(0).getImpressionId(),
+          feedback2.getSlots().get(0).getImpressionId()
+      );
+
+      return true;
+    }));
+  }
+
   private void assertRequestHeaderIsExpected(MetricRequest request) {
     assertEquals(buildConfigWrapper.getProfileId(), request.getProfileId());
     assertEquals(buildConfigWrapper.getSdkVersion(), request.getWrapperVersion());
@@ -156,6 +187,15 @@ public class CsmFunctionalTest {
   private void assertItRepresentsNoBid(MetricRequestFeedback feedback) {
     assertEquals(0, feedback.getCdbCallStartElapsed());
     assertNotNull(feedback.getCdbCallEndElapsed());
+    assertNull(feedback.getElapsed());
+    assertFalse(feedback.isTimeout());
+    assertEquals(1, feedback.getSlots().size());
+    assertFalse(feedback.getSlots().get(0).getCachedBidUsed());
+  }
+
+  private void assertItRepresentsNetworkError(MetricRequestFeedback feedback) {
+    assertEquals(0, feedback.getCdbCallStartElapsed());
+    assertNull(feedback.getCdbCallEndElapsed());
     assertNull(feedback.getElapsed());
     assertFalse(feedback.isTimeout());
     assertEquals(1, feedback.getSlots().size());
