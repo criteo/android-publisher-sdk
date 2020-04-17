f3ae648724ca9698ea283ebc0bb65e86ffe379cc
Amokrane Chentir <ma.chentir@criteo.com>
2020-04-17 11:34:58 +0200

    Bump to version 3.5.0
    JIRA: EE-883
    Change-Id: I0a8e874526e6327af33b20f8169777d28513d112

--- buildSrc/src/main/java/SdkVersion.kt
+++ buildSrc/src/main/java/SdkVersion.kt
@@ -2,7 +2,7 @@ import org.gradle.api.Project
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-private const val sdkBaseVersion = "3.4.0"
+private const val sdkBaseVersion = "3.5.0"
 
 private val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmm"))
 
