58e8b25ee868f542aa12c4d666ce85f76f99c409
p.turpin <p.turpin@criteo.com>
2020-05-05 13:52:06 +0200

    [Oups] Fix script to release on prod
    Change-Id: Ifc01b4a2b0c8bc28cced89731944e1ba6e98e8a5

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -5,9 +5,6 @@ set -xEeuo pipefail
 RELEASE_VERSION=$1
 RELEASE_VERSION_WITH_UNDERSCORE=${RELEASE_VERSION//./_}
 
-echo "$RELEASE_VERSION"
-exit 0
-
 git fetch origin
 git checkout remotes/origin/v$RELEASE_VERSION_WITH_UNDERSCORE
 
