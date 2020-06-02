e103b64951e4d222a843dc8bf9839373bf220f69
p.turpin <p.turpin@criteo.com>
2020-06-02 11:01:51 +0200

    Update Slack plugin version to 0.2.0
    This version change the default Git block format by providing a "git
    describe" of the HEAD instead of the current branch name. With the
    current CI workflow, HEAD is detached from the master branch (or any
    other branch) so the branch name is the commit SHA-1, which is
    misleading when we're cherry-picking commits across branches. With git
    describe, the nearest refs (branch or tag) of HEAD is retrieved and
    displayed.
    
    Change-Id: I281ce4fe8638a296bbf49a741ab38717320632fe

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
   implementation(gradleApi())
   implementation("com.android.tools.build:gradle:3.6.1")
   implementation("com.microsoft.azure:azure-storage:2.0.0")
-  implementation("gradle.plugin.fr.pturpin.slackpublish:slack-publish:0.1.0")
+  implementation("gradle.plugin.fr.pturpin.slackpublish:slack-publish:0.2.0")
 }
