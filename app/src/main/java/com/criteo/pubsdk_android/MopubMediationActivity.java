3e33ff694596f3cd3b1439ca202d1705f43e75ef
Amokrane Chentir <ma.chentir@criteo.com>
2020-06-17 14:23:10 +0200

    Reimport AdMob and MoPub dependencies
    JIRA: EE-1130
    Change-Id: I825e189ae04bd5464a6ccea5de0570d94780ba47

--- app/src/main/java/com/criteo/pubsdk_android/MopubMediationActivity.java
+++ app/src/main/java/com/criteo/pubsdk_android/MopubMediationActivity.java
@@ -11,7 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-//import com.criteo.mediation.mopub.advancednative.CriteoNativeEventRenderer;
+import com.criteo.mediation.mopub.advancednative.CriteoNativeEventRenderer;
 import com.criteo.pubsdk_android.listener.TestAppMoPubInterstitialAdListener;
 import com.mopub.mobileads.MoPubInterstitial;
 import com.mopub.mobileads.MoPubView;
@@ -66,7 +66,7 @@ public class MopubMediationActivity extends AppCompatActivity {
       }
     });
 
-//    moPubNative.registerAdRenderer(new CriteoNativeEventRenderer(new TestAppNativeRenderer()));
+    moPubNative.registerAdRenderer(new CriteoNativeEventRenderer(new TestAppNativeRenderer()));
   }
 
   private void onBannerClick() {
