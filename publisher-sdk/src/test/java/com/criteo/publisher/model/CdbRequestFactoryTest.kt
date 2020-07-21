c69182ad44016a7bf6d0e9deb27f260a29a96dbb
p.turpin <p.turpin@criteo.com>
2020-07-21 12:42:46 +0200

    Reformat CdbRequestFactoryTest.kt
    JIRA: EE-1198

--- publisher-sdk/src/test/java/com/criteo/publisher/model/CdbRequestFactoryTest.kt
+++ publisher-sdk/src/test/java/com/criteo/publisher/model/CdbRequestFactoryTest.kt
@@ -36,177 +36,177 @@ import java.util.concurrent.atomic.AtomicInteger
 
 class CdbRequestFactoryTest {
 
-    @Mock
-    private lateinit var publisher: Publisher
+  @Mock
+  private lateinit var publisher: Publisher
 
-    @Mock
-    private lateinit var deviceInfo: DeviceInfo
+  @Mock
+  private lateinit var deviceInfo: DeviceInfo
 
-    @Mock
-    private lateinit var advertisingInfo: AdvertisingInfo
+  @Mock
+  private lateinit var advertisingInfo: AdvertisingInfo
 
-    @Mock
-    private lateinit var userPrivacyUtil: UserPrivacyUtil
+  @Mock
+  private lateinit var userPrivacyUtil: UserPrivacyUtil
 
-    @Mock
-    private lateinit var uniqueIdGenerator: UniqueIdGenerator
+  @Mock
+  private lateinit var uniqueIdGenerator: UniqueIdGenerator
 
-    @Mock
-    private lateinit var buildConfigWrapper: BuildConfigWrapper
+  @Mock
+  private lateinit var buildConfigWrapper: BuildConfigWrapper
 
-    private lateinit var factory: CdbRequestFactory
+  private lateinit var factory: CdbRequestFactory
 
-    private val adUnitId = AtomicInteger()
+  private val adUnitId = AtomicInteger()
 
-    @Before
-    fun setUp() {
-        MockitoAnnotations.initMocks(this)
+  @Before
+  fun setUp() {
+    MockitoAnnotations.initMocks(this)
 
-        whenever(userPrivacyUtil.mopubConsent).thenReturn("mopubConsent")
-        whenever(userPrivacyUtil.iabUsPrivacyString).thenReturn("iabUsPrivacyString")
-        whenever(userPrivacyUtil.usPrivacyOptout).thenReturn("usPrivacyoptout")
+    whenever(userPrivacyUtil.mopubConsent).thenReturn("mopubConsent")
+    whenever(userPrivacyUtil.iabUsPrivacyString).thenReturn("iabUsPrivacyString")
+    whenever(userPrivacyUtil.usPrivacyOptout).thenReturn("usPrivacyoptout")
 
-        factory = CdbRequestFactory(
-            publisher,
-            deviceInfo,
-            advertisingInfo,
-            userPrivacyUtil,
-            uniqueIdGenerator,
-            buildConfigWrapper
-        )
+    factory = CdbRequestFactory(
+        publisher,
+        deviceInfo,
+        advertisingInfo,
+        userPrivacyUtil,
+        uniqueIdGenerator,
+        buildConfigWrapper
+    )
+  }
+
+  @Test
+  fun userAgent_GivenDeviceInfo_DelegateToIt() {
+    val expected: Future<String> = mock()
+    whenever(deviceInfo.userAgent).thenReturn(expected)
+
+    val userAgent = factory.userAgent
+
+    assertThat(userAgent).isSameAs(expected)
+  }
+
+
+  @Test
+  fun createRequest_GivenInput_BuildRequest() {
+    val adUnit = createAdUnit()
+    val adUnits: List<CacheAdUnit> = listOf(adUnit)
+    val expectedGdpr: GdprData = mock()
+
+    val expectedSlot = CdbRequestSlot.create(
+        "impId",
+        adUnit.placementId,
+        adUnit.adUnitType,
+        adUnit.size
+    )
+
+    buildConfigWrapper.stub {
+      on { sdkVersion } doReturn "1.2.3"
+      on { profileId } doReturn 42
     }
 
-    @Test
-    fun userAgent_GivenDeviceInfo_DelegateToIt() {
-        val expected: Future<String> = mock()
-        whenever(deviceInfo.userAgent).thenReturn(expected)
+    whenever(userPrivacyUtil.gdprData).thenReturn(expectedGdpr)
+    whenever(uniqueIdGenerator.generateId()).thenReturn("impId")
 
-        val userAgent = factory.userAgent
+    val request = factory.createRequest(adUnits)
 
-        assertThat(userAgent).isSameAs(expected)
+    assertThat(request.publisher).isEqualTo(publisher)
+    assertThat(request.sdkVersion).isEqualTo("1.2.3")
+    assertThat(request.profileId).isEqualTo(42)
+    assertThat(request.gdprData).isEqualTo(expectedGdpr)
+    assertThat(request.slots).containsExactlyInAnyOrder(expectedSlot)
+  }
+
+  @Test
+  fun givenOneRequestWithNonEmptyPrivacyValue_AndOneRequestWithEmptyPrivacyValues_VerifyRequestsAreDifferent() {
+    // request 1
+    val adUnit = createAdUnit()
+    val adUnits: List<CacheAdUnit> = listOf(adUnit)
+    val expectedGdpr: GdprData = mock()
+
+    val expectedSlot = CdbRequestSlot.create(
+        "impId",
+        adUnit.placementId,
+        adUnit.adUnitType,
+        adUnit.size
+    )
+
+    buildConfigWrapper.stub {
+      on { sdkVersion } doReturn "1.2.3"
+      on { profileId } doReturn 1337
     }
 
-
-    @Test
-    fun createRequest_GivenInput_BuildRequest() {
-        val adUnit = createAdUnit()
-        val adUnits: List<CacheAdUnit> = listOf(adUnit)
-        val expectedGdpr: GdprData = mock()
-
-        val expectedSlot = CdbRequestSlot.create(
-            "impId",
-            adUnit.placementId,
-            adUnit.adUnitType,
-            adUnit.size
-        )
-
-        buildConfigWrapper.stub {
-            on { sdkVersion } doReturn "1.2.3"
-            on { profileId } doReturn 42
-        }
-
-        whenever(userPrivacyUtil.gdprData).thenReturn(expectedGdpr)
-        whenever(uniqueIdGenerator.generateId()).thenReturn("impId")
-
-        val request = factory.createRequest(adUnits)
-
-        assertThat(request.publisher).isEqualTo(publisher)
-        assertThat(request.sdkVersion).isEqualTo("1.2.3")
-        assertThat(request.profileId).isEqualTo(42)
-        assertThat(request.gdprData).isEqualTo(expectedGdpr)
-        assertThat(request.slots).containsExactlyInAnyOrder(expectedSlot)
+    userPrivacyUtil.stub {
+      on { gdprData } doReturn expectedGdpr
+      on { usPrivacyOptout } doReturn "usPrivacyOptout"
+      on { iabUsPrivacyString } doReturn "iabUsPrivacyString"
+      on { mopubConsent } doReturn "mopubConsent"
     }
 
-    @Test
-    fun givenOneRequestWithNonEmptyPrivacyValue_AndOneRequestWithEmptyPrivacyValues_VerifyRequestsAreDifferent() {
-        // request 1
-        val adUnit = createAdUnit()
-        val adUnits: List<CacheAdUnit> = listOf(adUnit)
-        val expectedGdpr: GdprData = mock()
+    whenever(uniqueIdGenerator.generateId()).thenReturn("impId")
 
-        val expectedSlot = CdbRequestSlot.create(
-                "impId",
-                adUnit.placementId,
-                adUnit.adUnitType,
-                adUnit.size
-        )
+    var request = factory.createRequest(adUnits)
 
-        buildConfigWrapper.stub {
-            on { sdkVersion } doReturn "1.2.3"
-            on { profileId } doReturn 1337
-        }
+    assertThat(request.publisher).isEqualTo(publisher)
+    assertThat(request.sdkVersion).isEqualTo("1.2.3")
+    assertThat(request.profileId).isEqualTo(1337)
+    assertThat(request.gdprData).isEqualTo(expectedGdpr)
+    assertThat(request.user.uspIab()).isEqualTo("iabUsPrivacyString")
+    assertThat(request.user.uspOptout()).isEqualTo("usPrivacyOptout")
+    assertThat(request.user.mopubConsent()).isEqualTo("mopubConsent")
+    assertThat(request.slots).containsExactlyInAnyOrder(expectedSlot)
 
-        userPrivacyUtil.stub {
-            on { gdprData } doReturn expectedGdpr
-            on { usPrivacyOptout } doReturn "usPrivacyOptout"
-            on { iabUsPrivacyString } doReturn "iabUsPrivacyString"
-            on { mopubConsent } doReturn "mopubConsent"
-        }
-
-        whenever(uniqueIdGenerator.generateId()).thenReturn("impId")
-
-        var request = factory.createRequest(adUnits)
-
-        assertThat(request.publisher).isEqualTo(publisher)
-        assertThat(request.sdkVersion).isEqualTo("1.2.3")
-        assertThat(request.profileId).isEqualTo(1337)
-        assertThat(request.gdprData).isEqualTo(expectedGdpr)
-        assertThat(request.user.uspIab()).isEqualTo("iabUsPrivacyString")
-        assertThat(request.user.uspOptout()).isEqualTo("usPrivacyOptout")
-        assertThat(request.user.mopubConsent()).isEqualTo("mopubConsent")
-        assertThat(request.slots).containsExactlyInAnyOrder(expectedSlot)
-
-        // request 2
-        userPrivacyUtil.stub {
-            on { usPrivacyOptout } doReturn ""
-            on { iabUsPrivacyString } doReturn ""
-            on { mopubConsent } doReturn ""
-        }
-
-        request = factory.createRequest(adUnits)
-
-        assertThat(request.user.uspIab()).isNull()
-        assertThat(request.user.uspOptout()).isNull()
-        assertThat(request.user.mopubConsent()).isNull()
+    // request 2
+    userPrivacyUtil.stub {
+      on { usPrivacyOptout } doReturn ""
+      on { iabUsPrivacyString } doReturn ""
+      on { mopubConsent } doReturn ""
     }
 
-    @Test
-    fun createRequest_GivenAdUnits_MapThemToRequestSlotWithImpressionId() {
-        val adUnit1 = createAdUnit()
-        val adUnit2 = createAdUnit()
-        val adUnits: List<CacheAdUnit> = listOf(adUnit1, adUnit2)
+    request = factory.createRequest(adUnits)
 
-        val expectedSlot1 = CdbRequestSlot.create(
-            "impId1",
-            adUnit1.placementId,
-            adUnit1.adUnitType,
-            adUnit1.size
-        )
+    assertThat(request.user.uspIab()).isNull()
+    assertThat(request.user.uspOptout()).isNull()
+    assertThat(request.user.mopubConsent()).isNull()
+  }
 
-        val expectedSlot2 = CdbRequestSlot.create(
-            "impId2",
-            adUnit2.placementId,
-            adUnit2.adUnitType,
-            adUnit2.size
-        )
+  @Test
+  fun createRequest_GivenAdUnits_MapThemToRequestSlotWithImpressionId() {
+    val adUnit1 = createAdUnit()
+    val adUnit2 = createAdUnit()
+    val adUnits: List<CacheAdUnit> = listOf(adUnit1, adUnit2)
 
-        uniqueIdGenerator.stub {
-            on { generateId() }.doReturn("impId1", "impId2")
-        }
+    val expectedSlot1 = CdbRequestSlot.create(
+        "impId1",
+        adUnit1.placementId,
+        adUnit1.adUnitType,
+        adUnit1.size
+    )
 
-        buildConfigWrapper.stub {
-            on { sdkVersion } doReturn "1.2.3"
-            on { profileId } doReturn 1337
-        }
+    val expectedSlot2 = CdbRequestSlot.create(
+        "impId2",
+        adUnit2.placementId,
+        adUnit2.adUnitType,
+        adUnit2.size
+    )
 
-        val request = factory.createRequest(adUnits)
-
-        assertThat(request.slots).containsExactlyInAnyOrder(expectedSlot1, expectedSlot2)
+    uniqueIdGenerator.stub {
+      on { generateId() }.doReturn("impId1", "impId2")
     }
 
-    private fun createAdUnit(): CacheAdUnit {
-        val id = "adUnit #" + adUnitId.incrementAndGet()
-        return CacheAdUnit(AdSize(1, 2), id, CRITEO_BANNER)
+    buildConfigWrapper.stub {
+      on { sdkVersion } doReturn "1.2.3"
+      on { profileId } doReturn 1337
     }
+
+    val request = factory.createRequest(adUnits)
+
+    assertThat(request.slots).containsExactlyInAnyOrder(expectedSlot1, expectedSlot2)
+  }
+
+  private fun createAdUnit(): CacheAdUnit {
+    val id = "adUnit #" + adUnitId.incrementAndGet()
+    return CacheAdUnit(AdSize(1, 2), id, CRITEO_BANNER)
+  }
 
 }
