74ddacad69c066fee1fa9f186a80995912768942
p.turpin <p.turpin@criteo.com>
2020-07-24 13:13:59 +0200

    Inject mock integration detector configured by activity
    JIRA: EE-1202

--- app/src/main/java/com/criteo/pubsdk_android/integration/IntegrationSelectorActivity.kt
+++ app/src/main/java/com/criteo/pubsdk_android/integration/IntegrationSelectorActivity.kt
@@ -17,13 +17,56 @@
 package com.criteo.pubsdk_android.integration
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
+import com.criteo.publisher.DependencyProvider
+import com.criteo.publisher.integration.IntegrationDetector
 import com.criteo.pubsdk_android.R
+import com.criteo.pubsdk_android.mock.MockedDependencyProvider
 
 class IntegrationSelectorActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_integration_selector)
+
+    findViewById<Button>(R.id.btnSaveIntegrationDetectionValues).setOnClickListener {
+      commitConfiguration()
+    }
+  }
+
+  private fun commitConfiguration() {
+    val verbose = findViewById<CheckBox>(R.id.checkboxVerboseIntegrationDetection).isChecked
+    val noMock = findViewById<RadioButton>(R.id.radioNoMock).isChecked
+    val allMocked = findViewById<RadioButton>(R.id.radioAllMocked).isChecked
+    val noMediation = findViewById<RadioButton>(R.id.radioNoMediation).isChecked
+
+    if (!verbose && noMock) {
+      MockedDependencyProvider.stopMocking()
+    } else {
+      val sharedPreferences = DependencyProvider.getInstance().provideSharedPreferences()
+
+      MockedDependencyProvider.startMocking {
+        val integrationDetector = if (noMediation) {
+          NoIntegrationDetector()
+        } else {
+          IntegrationDetector()
+        }
+
+        val integrationRegistry = MockedIntegrationRegistry(
+            sharedPreferences,
+            integrationDetector,
+            this@IntegrationSelectorActivity,
+            allMocked,
+            verbose
+        )
+
+        inject(integrationDetector)
+        inject(integrationRegistry)
+      }
+    }
   }
 }
+
