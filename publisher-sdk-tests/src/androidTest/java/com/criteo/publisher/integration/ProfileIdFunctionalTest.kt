04cc6dc2d16c2fb93f945baf324777d9b7feb92a
p.turpin <p.turpin@criteo.com>
2020-07-28 08:50:53 +0200

    Add E2E tests on prefetch for Profile ID
    # Tested scenarios:
    ## Prefetch done from unused SDK
    - Given SDK used for the first time
    - When initializing the SDK with ad units in prefetch
    - And waiting for idle state
    - Then CDB call is done with fallback profile ID
    
    ## Prefetch done from used SDK
    - Given initialized SDK
    - Given bid response fetched for InHouse (or any other integration)
    - Given SDK rebooted (in-memory data is flush)
    - When initializing the SDK with ad units in prefetch
    - And waiting for idle state
    - Then CDB call is done with InHouse (or other) profile ID
    
    JIRA: EE-1205

---
+++ publisher-sdk-tests/src/androidTest/java/com/criteo/publisher/integration/ProfileIdFunctionalTest.kt
@@ -0,0 +1,67 @@
+/*
+ *    Copyright 2020 Criteo
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.criteo.publisher.integration
+
+import com.criteo.publisher.Criteo
+import com.criteo.publisher.CriteoUtil.givenInitializedCriteo
+import com.criteo.publisher.TestAdUnits.BANNER_320_480
+import com.criteo.publisher.mock.MockedDependenciesRule
+import com.criteo.publisher.mock.SpyBean
+import com.criteo.publisher.network.PubSdkApi
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.check
+import com.nhaarman.mockitokotlin2.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+
+class ProfileIdFunctionalTest {
+
+  @Rule
+  @JvmField
+  val mockedDependenciesRule = MockedDependenciesRule()
+
+  @SpyBean
+  private lateinit var api: PubSdkApi
+
+  @Test
+  fun prefetch_GivenSdkUsedForTheFirstTime_UseFallbackProfileId() {
+    givenInitializedCriteo(BANNER_320_480)
+    mockedDependenciesRule.waitForIdleState()
+
+    verify(api).loadCdb(check {
+      assertThat(it.profileId).isEqualTo(Integration.FALLBACK.profileId)
+    }, any())
+  }
+
+  @Test
+  fun prefetch_GivenUsedSdk_UseLatestProfileId() {
+    givenInitializedCriteo()
+    Criteo.getInstance().getBidResponse(BANNER_320_480)
+    mockedDependenciesRule.waitForIdleState()
+
+    mockedDependenciesRule.resetAllDependencies()
+
+    givenInitializedCriteo(BANNER_320_480)
+    mockedDependenciesRule.waitForIdleState()
+
+    verify(api).loadCdb(check {
+      assertThat(it.profileId).isEqualTo(Integration.IN_HOUSE.profileId)
+    }, any())
+  }
+
+}
