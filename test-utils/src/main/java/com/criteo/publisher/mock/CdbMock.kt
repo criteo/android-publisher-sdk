901a0e038d3eef76d25b14f6eed272802278cbc4
p.turpin <p.turpin@criteo.com>
2020-08-10 15:24:10 +0200

    Extract inner class from CDB mock server
    By default, detekt accepts classes with at most 11 methods. CdbMock was
    over the limit. Hence it is now split in two classes: the server setup
    and the query handler.
    
    JIRA: EE-1214

--- test-utils/src/main/java/com/criteo/publisher/mock/CdbMock.kt
+++ test-utils/src/main/java/com/criteo/publisher/mock/CdbMock.kt
@@ -59,23 +59,7 @@ class CdbMock(private val jsonSerializer: JsonSerializer) {
     }
 
   fun start() {
-    mockWebServer.dispatcher = object: Dispatcher() {
-      override fun dispatch(request: RecordedRequest): MockResponse {
-        return when (request.requestUrl?.encodedPath) {
-          "/csm" -> handleCsmRequest()
-          "/config/app" -> handleConfigRequest()
-          "/inapp/v2" -> handleBidRequest(request.body)
-          "/delivery/ajs.php" -> handleCasperRequest(request)
-          "/appevent/v1/2379" -> handleBearcatRequest()
-          else -> MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
-        }.also {
-          if (simulateSlowNetwork.compareAndSet(true, false)) {
-            it.throttleBody(1, 1, TimeUnit.SECONDS)
-          }
-        }
-      }
-    }
-
+    mockWebServer.dispatcher = CdbMockHandler()
     mockWebServer.start()
   }
 
@@ -83,23 +67,40 @@ class CdbMock(private val jsonSerializer: JsonSerializer) {
     simulateSlowNetwork.set(true)
   }
 
-  private fun handleBearcatRequest(): MockResponse {
-    return MockResponse().setHeader("content-type", "text/html")
-  }
-
   fun shutdown() {
     mockWebServer.shutdown()
   }
 
-  private fun handleCsmRequest(): MockResponse {
-    return MockResponse().setStatus("HTTP/1.1 204 No Content")
-  }
+  private inner class CdbMockHandler : Dispatcher() {
+    override fun dispatch(request: RecordedRequest): MockResponse {
+      return when (request.requestUrl?.encodedPath) {
+        "/csm" -> handleCsmRequest()
+        "/config/app" -> handleConfigRequest()
+        "/inapp/v2" -> handleBidRequest(request.body)
+        "/delivery/ajs.php" -> handleCasperRequest(request)
+        "/appevent/v1/2379" -> handleBearcatRequest()
+        else -> MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
+      }.also {
+        if (simulateSlowNetwork.compareAndSet(true, false)) {
+          it.throttleBody(1, 1, TimeUnit.SECONDS)
+        }
+      }
+    }
 
-  @Suppress("MaxLineLength")
-  private fun handleConfigRequest(): MockResponse {
-    return MockResponse()
-        .setHeader("content-type", "application/json; charset=utf-8")
-        .setBody("""
+    private fun handleBearcatRequest(): MockResponse {
+      return MockResponse().setHeader("content-type", "text/html")
+    }
+
+    private fun handleCsmRequest(): MockResponse {
+      return MockResponse().setStatus("HTTP/1.1 204 No Content")
+    }
+
+    @Suppress("MaxLineLength")
+    private fun handleConfigRequest(): MockResponse {
+      return MockResponse()
+          .setHeader("content-type", "application/json; charset=utf-8")
+          .setBody(
+              """
             {
               "killSwitch": false,
               "AndroidDisplayUrlMacro": "%%displayUrl%%",
@@ -111,45 +112,46 @@ class CdbMock(private val jsonSerializer: JsonSerializer) {
               "iOSAdTagUrlMode": "<!doctype html><html><head><meta charset=\"utf-8\"><style>body{margin:0;padding:0}</style><meta name=\"viewport\" content=\"width=%%width%%, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no\" ></head><body><script src=\"%%displayUrl%%\"></script></body></html>",
               "csmEnabled": true
             }
-          """)
-  }
-
-  private fun handleBidRequest(body: Buffer): MockResponse {
-    val cdbRequest = jsonSerializer.read(CdbRequest::class.java, body.inputStream())
-
-    if (shouldNotBid(cdbRequest)) {
-      return MockResponse().setResponseCode(HttpURLConnection.HTTP_NO_CONTENT)
+          """
+          )
     }
 
-    val responseSlots = cdbRequest.slots.mapNotNull { it.toResponseSlot() }.joinToString()
-    val requestId = UUID.randomUUID().toString()
-    val cdbResponse = """
+    private fun handleBidRequest(body: Buffer): MockResponse {
+      val cdbRequest = jsonSerializer.read(CdbRequest::class.java, body.inputStream())
+
+      if (shouldNotBid(cdbRequest)) {
+        return MockResponse().setResponseCode(HttpURLConnection.HTTP_NO_CONTENT)
+      }
+
+      val responseSlots = cdbRequest.slots.mapNotNull { it.toResponseSlot() }.joinToString()
+      val requestId = UUID.randomUUID().toString()
+      val cdbResponse = """
       {
         "slots": [$responseSlots],
         "requestId":"$requestId"
       }
     """.trimIndent()
 
-    return MockResponse()
-        .setHeader("content-type", "application/json; charset=utf-8")
-        .setBody(cdbResponse)
-  }
+      return MockResponse()
+          .setHeader("content-type", "application/json; charset=utf-8")
+          .setBody(cdbResponse)
+    }
 
-  private fun shouldNotBid(cdbRequest: CdbRequest) =
-      cdbRequest.gdprData?.consentData() in setOf(TCF1_CONSENT_NOT_GIVEN, TCF2_CONSENT_NOT_GIVEN)
+    private fun shouldNotBid(cdbRequest: CdbRequest) =
+        cdbRequest.gdprData?.consentData() in setOf(TCF1_CONSENT_NOT_GIVEN, TCF2_CONSENT_NOT_GIVEN)
 
-  private fun CdbRequestSlot.toResponseSlot(): String? {
-    val size = sizes.first().toAdSize()
-    val width = size.width
-    val height = size.height
+    private fun CdbRequestSlot.toResponseSlot(): String? {
+      val size = sizes.first().toAdSize()
+      val width = size.width
+      val height = size.height
 
-    val bannerAdUnit = BannerAdUnit(placementId, size)
-    val interstitialAdUnit = InterstitialAdUnit(placementId)
-    val nativeAdUnit = NativeAdUnit(placementId)
+      val bannerAdUnit = BannerAdUnit(placementId, size)
+      val interstitialAdUnit = InterstitialAdUnit(placementId)
+      val nativeAdUnit = NativeAdUnit(placementId)
 
-    return when {
-      bannerAdUnit == BANNER_320_50 || bannerAdUnit == BANNER_320_480 || interstitialAdUnit == INTERSTITIAL -> {
-        """
+      return when {
+        bannerAdUnit == BANNER_320_50 || bannerAdUnit == BANNER_320_480 || interstitialAdUnit == INTERSTITIAL -> {
+          """
         {
           "impId": "$impressionId",
           "placementId": "$placementId",
@@ -163,9 +165,9 @@ class CdbMock(private val jsonSerializer: JsonSerializer) {
           "displayUrl": "$url/delivery/ajs.php?width=$width&height=$height"
         }
       """.trimIndent()
-      }
-      nativeAdUnit == NATIVE -> {
-        """
+        }
+        nativeAdUnit == NATIVE -> {
+          """
         {
           "impId": "$impressionId",
           "placementId": "$placementId",
@@ -179,23 +181,23 @@ class CdbMock(private val jsonSerializer: JsonSerializer) {
           "native": $STUB_NATIVE_JSON
         }
       """.trimIndent()
+        }
+        else -> null
       }
-      else -> null
     }
-  }
 
-  private fun String.toAdSize(): AdSize {
-    val split = split("x")
-    val width = split[0].toInt()
-    val height = split[1].toInt()
-    return AdSize(width, height)
-  }
+    private fun String.toAdSize(): AdSize {
+      val split = split("x")
+      val width = split[0].toInt()
+      val height = split[1].toInt()
+      return AdSize(width, height)
+    }
 
-  private fun handleCasperRequest(request: RecordedRequest): MockResponse {
-    val width = request.requestUrl!!.queryParameter("width")!!.toInt()
-    val height = request.requestUrl!!.queryParameter("height")!!.toInt()
+    private fun handleCasperRequest(request: RecordedRequest): MockResponse {
+      val width = request.requestUrl!!.queryParameter("width")!!.toInt()
+      val height = request.requestUrl!!.queryParameter("height")!!.toInt()
 
-    val response = """
+      val response = """
       (function(){
       var s = "";
       s += "<"+"a href=\"https://criteo.com\">\n";
@@ -205,8 +207,9 @@ class CdbMock(private val jsonSerializer: JsonSerializer) {
       document.write(s);})();
     """.trimIndent()
 
-    return MockResponse()
-        .setHeader("content-type", "text/plain; charset=utf-8")
-        .setBody(response)
+      return MockResponse()
+          .setHeader("content-type", "text/plain; charset=utf-8")
+          .setBody(response)
+    }
   }
 }
