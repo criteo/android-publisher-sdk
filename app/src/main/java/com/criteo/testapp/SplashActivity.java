9d1e766b913632a66a3925b84a920b1c95cf1e8d
p.turpin <p.turpin@criteo.com>
2020-08-17 17:51:30 +0200

    Remove hand-made delayed task
    Using Thread object by hand is really discouraged because it is hard to
    handle all edge-cases: error, interruption, starvation, ...
    Moreover, this is the typical use case of Android handlers.

--- app/src/main/java/com/criteo/testapp/SplashActivity.java
+++ app/src/main/java/com/criteo/testapp/SplashActivity.java
@@ -18,6 +18,7 @@ package com.criteo.testapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,25 +31,16 @@ public class SplashActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    this.getWindow().setFlags(
+        WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN
+    );
     setContentView(R.layout.activity_splash);
 
-    Thread timer = new Thread() {
-      public void run() {
-        try {
-          sleep(SPLASH_TIME);
-        } catch (Exception e) {
-          e.printStackTrace();
-        } finally {
-          Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-          startActivity(intent);
-
-        }
-      }
-    };
-    timer.start();
-
+    new Handler().postDelayed(() -> {
+      Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+      startActivity(intent);
+    }, SPLASH_TIME);
   }
 
 }
