3060b4e583e32d977e4ac93927c7f72cbb73e0a5
p.turpin <p.turpin@criteo.com>
2020-08-18 09:58:01 +0200

    Set interruption flag when interrupted while waiting for threads
    This code still throw only a runtime exception instead of an interrupted
    exception so it is convenient to call. Also, this code is only called
    from tests who, normally, dont care about interruptions.

---
+++ test-utils/src/androidTest/java/com/criteo/publisher/concurrent/ThreadingUtilAndroidTest.kt
@@ -0,0 +1,96 @@
+/*
+ *    Copyright 2020 Criteo
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.criteo.publisher.concurrent
+
+import android.os.Handler
+import android.os.Looper
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatCode
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicBoolean
+
+class ThreadingUtilAndroidTest {
+
+  @Test(timeout = 100)
+  fun waitForMessageQueueToBeIdle_GivenNoJob_ReturnImmediately() {
+    ThreadingUtil.waitForMessageQueueToBeIdle()
+
+    // no assertion, the timeout is validating this test
+  }
+
+  @Test
+  fun waitForMessageQueueToBeIdle_GivenJobPostedOnUiThreadBefore_WaitForIt() {
+    val isSetOnUiThread = AtomicBoolean(false)
+    val uiJobIsBeingExecuted = CountDownLatch(1)
+
+    val handler = Handler(Looper.getMainLooper())
+    handler.post {
+      uiJobIsBeingExecuted.countDown()
+      simulateUiWork()
+      isSetOnUiThread.set(true)
+    }
+
+    uiJobIsBeingExecuted.await()
+    ThreadingUtil.waitForMessageQueueToBeIdle()
+
+    assertThat(isSetOnUiThread).isTrue
+  }
+
+  @Test
+  fun waitForMessageQueueToBeIdle_GivenJobPostingAnotherOneOnUiThread_WaitForAllJobs() {
+    val isSetInSecondJob = AtomicBoolean(false)
+    val firstJobIsBeingExecuted = CountDownLatch(1)
+
+    val handler = Handler(Looper.getMainLooper())
+
+    handler.post {
+      firstJobIsBeingExecuted.countDown()
+      simulateUiWork()
+
+      handler.post {
+        simulateUiWork()
+        isSetInSecondJob.set(true)
+      }
+    }
+
+    firstJobIsBeingExecuted.await()
+    ThreadingUtil.waitForMessageQueueToBeIdle()
+
+    assertThat(isSetInSecondJob).isTrue
+  }
+
+  @Test
+  fun waitForMessageQueueToBeIdle_GivenInterruptedWaitingThread_AbortWaitingByThrowingRuntimeAndSetInterruptionFlag() {
+    Thread.currentThread().interrupt()
+
+    assertThatCode {
+      ThreadingUtil.waitForMessageQueueToBeIdle()
+    }.hasCauseInstanceOf(InterruptedException::class.java)
+
+    assertThat(Thread.currentThread().isInterrupted).isTrue()
+  }
+
+  /**
+   * Simulate some works that may be done on UI thread.
+   *
+   * This shows that the [ThreadingUtil.waitForMessageQueueToBeIdle] method will wait if UI jobs
+   * have work to do.
+   */
+  private fun simulateUiWork() {
+    Thread.sleep(50)
+  }
+}
--- test-utils/src/main/java/com/criteo/publisher/concurrent/ThreadingUtil.java
+++ test-utils/src/main/java/com/criteo/publisher/concurrent/ThreadingUtil.java
@@ -84,6 +84,18 @@ public class ThreadingUtil {
     }
   }
 
+  /**
+   * Wait for all threads in the given executor and for all task on the UI thread too.
+   * <p>
+   * This method is also working when called on a Java VM (outside Android then), and as there is no
+   * concept of UI thread, only commands in given executor are waited for.
+   * <p>
+   * If the current thread get interrupted while waiting, then this method ends earlier by throwing
+   * a runtime exception instead of an {@link InterruptedException} just to be convenient for
+   * callers. Although, the interruption flag on this thread is set on.
+   *
+   * @param trackingCommandsExecutor executor to wait commands from
+   */
   @RequiresApi(api = VERSION_CODES.M)
   public static void waitForAllThreads(@NonNull TrackingCommandsExecutor trackingCommandsExecutor) {
     // FIXME EE-764 This is a wait with two different steps (main and async). Because of those steps, it
@@ -103,6 +115,7 @@ public class ThreadingUtil {
     try {
       trackingCommandsExecutor.waitCommands();
     } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
       throw new RuntimeException(e);
     }
 
@@ -114,6 +127,10 @@ public class ThreadingUtil {
   /**
    * Blocks the current threads until the {@link MessageQueue} associated to the provided {@link
    * Looper} is idle.
+   * <p>
+   * If the current thread get interrupted while waiting, then this method ends earlier by throwing
+   * a runtime exception instead of an {@link InterruptedException} just to be convenient for
+   * callers. Although, the interruption flag on this thread is set on.
    */
   @RequiresApi(api = VERSION_CODES.M)
   public static void waitForMessageQueueToBeIdle() {
@@ -136,6 +153,7 @@ public class ThreadingUtil {
     try {
       latch.await();
     } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
       throw new RuntimeException(e);
     }
   }
