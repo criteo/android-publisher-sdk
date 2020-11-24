2274863e7b7ee7af1c5d4483af6b64dd14a66646
p.turpin <p.turpin@criteo.com>
2020-11-24 11:07:03 +0100

    Fix dependency injection of beans and logger
    When `withSpiedLogger` option is used in test, this fix ensure that all
    beans are correctly spied/mocked/injected, that the logger is properly
    spied with the proper dependencies.
    
    JIRA: EE-1343

---
+++ publisher-sdk/src/test/java/com/criteo/publisher/mock/MockedDependenciesRuleLoggingTest.kt
@@ -0,0 +1,80 @@
+/*
+ *    Copyright 2020 Criteo
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.criteo.publisher.mock
+
+import com.criteo.publisher.DependencyProvider
+import com.criteo.publisher.annotation.OpenForTesting
+import com.criteo.publisher.logging.ConsoleHandler
+import com.criteo.publisher.logging.LogMessage
+import com.criteo.publisher.logging.LoggerFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.ObjectAssert
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+
+class MockedDependenciesRuleLoggingTest {
+
+  @Rule
+  @JvmField
+  val mockedDependenciesRule = object : MockedDependenciesRule() {
+    override fun createDependencyProvider() = TestedDependencyProvider()
+  }.withSpiedLogger()
+
+  @SpyBean
+  private lateinit var beansWithLogger: BeansWithLogger
+
+  @SpyBean
+  private lateinit var consoleHandler: ConsoleHandler
+
+  @Test
+  fun withSpiedLogger_InjectSpiedLoggerAndBeansProperly() {
+    assertThat(beansWithLogger.logger).isNotNull.isSameAs(mockedDependenciesRule.spiedLogger)
+    assertThat(consoleHandler).isSpy()
+    assertThat(beansWithLogger).isSpy()
+  }
+
+  @Test
+  fun withSpiedLogger_WhenLogging_DelegateToInjectedConsoleHandler() {
+    val logMessage = LogMessage(message = "dummy")
+
+    mockedDependenciesRule.spiedLogger!!.log(logMessage)
+
+    verify(consoleHandler).log(any(), eq(logMessage))
+  }
+
+  private fun <T> ObjectAssert<T>.isSpy(): ObjectAssert<T> {
+    return matches {
+      Mockito.mockingDetails(it).isSpy
+    }
+  }
+
+  @OpenForTesting
+  class TestedDependencyProvider : DependencyProvider() {
+    fun provideBeansWithLogger(): BeansWithLogger {
+      return getOrCreate(BeansWithLogger::class.java, ::BeansWithLogger)
+    }
+  }
+
+  @OpenForTesting
+  class BeansWithLogger {
+    val logger = LoggerFactory.getLogger(javaClass)
+  }
+}
