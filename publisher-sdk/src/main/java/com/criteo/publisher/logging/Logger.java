b916c1edd50d989d92f41eef154e8f40b83bd160
p.turpin <p.turpin@criteo.com>
2020-12-07 19:30:42 +0100

    Protect infinite recursion from handlers that use logger
    The `AdvertiserUtil` can log a failure when getting the GAID throws an
    exception.
    The `IntegrationRegistry` class can log a failure when shared preference
    contains an ill-formed integration name.
    
    As they are used by the RemoteHandler to get context of remote logs, an
    infinite recursive loop might be initiated. The logger should then
    detect it and stop it.
    
    Such errors can be very uncommon but if they are occurring once, they
    might lock the SDK on the device.
    
    JIRA: EE-1347

--- publisher-sdk/src/main/java/com/criteo/publisher/logging/Logger.java
+++ publisher-sdk/src/main/java/com/criteo/publisher/logging/Logger.java
@@ -31,6 +31,46 @@ public class Logger {
   @NonNull
   private final List<LogHandler> handlers;
 
+  /**
+   * Indicate if one handler is logging another message and prevent infinite recursive loop.
+   *
+   * Handlers can have 1 depth of allowed logs. This means that recursive logs after 1 depth are skipped. For instance:
+   * <pre><code>
+   *   handler H is logging "foo" when handling a log message
+   *
+   *   - an actor logs a message "bar"
+   *   - logger gives "bar" to handler H
+   *   - handler H handles "bar"
+   *   - handler H logs "foo" (looping to logger)
+   *   - logger gives "foo" to handler H
+   *   - handler H handles "foo"
+   *   - handler H logs "foo"
+   *   - logger detects recursive loop and skips this log
+   * </code></pre>
+   *
+   * This integer contains the depth of the current logging situation:
+   * <ul>
+   *   <li>0 means that nothing is being logged: logger can log</li>
+   *   <li>1 means that a log is being handled: logger can log</li>
+   *   <li>2 means that a handler log is being handled: logger skip the log</li>
+   * </ul>
+   *
+   * Note that this protection is only done locally to a thread to avoid {@link StackOverflowError}. If this scenario
+   * happens between threads, then something really wrong is happening. Also, it is done across all loggers, so this
+   * protection is working when multiple loggers are used, and when new loggers are newly created.
+   *
+   * Checking for recursive logs is necessary because logging can throw an exception (for example when performing an I/O
+   * operation to build a log message). Throwing an exception will result in an additional log message, which can throw
+   * again... hence resulting in an infinite loop.
+   */
+  @NonNull
+  private static final ThreadLocal<Integer> logRecursionDepth = new ThreadLocal<Integer>() {
+    @Override
+    protected Integer initialValue() {
+      return 0;
+    }
+  };
+
   public Logger(
       @NonNull Class<?> klass,
       @NonNull List<LogHandler> handlers
@@ -60,11 +100,24 @@ public class Logger {
   }
 
   public void log(@NonNull LogMessage logMessage) {
+    int depth = logRecursionDepth.get();
+    if (depth > 1) {
+      return;
+    }
+
     for (LogHandler handler : handlers) {
+      logRecursionDepth.set(depth + 1);
       try {
         handler.log(tag, logMessage);
       } catch (Exception e) {
         Log.w(FALLBACK_TAG, "Impossible to log with handler: " + handler.getClass(), e);
+      } finally {
+        if (depth == 0) {
+          // See Sonar S5164: Clean current thread state to avoid memory leaks
+          logRecursionDepth.remove();
+        } else {
+          logRecursionDepth.set(depth);
+        }
       }
     }
   }
--- publisher-sdk/src/test/java/com/criteo/publisher/logging/LoggerTest.kt
+++ publisher-sdk/src/test/java/com/criteo/publisher/logging/LoggerTest.kt
@@ -18,6 +18,9 @@ package com.criteo.publisher.logging
 
 import android.util.Log
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.inOrder
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
@@ -81,5 +84,93 @@ class LoggerTest {
     logger.log(logMessage)
 
     verify(handler2).log("myTag", logMessage)
+  }
+
+  @Test
+  fun log_GivenLogsProducedWhileLogging_LogItOnlyOnceAndStopRecursion() {
+    lateinit var logger: Logger
+
+    val handler = mock<LogHandler>()
+
+    handler1 = object : LogHandler {
+      override fun log(tag: String, logMessage: LogMessage) {
+        logger.debug("start handler1#log")
+        handler.log(tag, logMessage.copy(message = "handler1: ${logMessage.message}"))
+        logger.debug("end handler1#log")
+      }
+    }
+
+    handler2 = object : LogHandler {
+      override fun log(tag: String, logMessage: LogMessage) {
+        logger.debug("start handler2#log")
+        handler.log(tag, logMessage.copy(message = "handler2: ${logMessage.message}"))
+        logger.debug("end handler2#log")
+      }
+    }
+
+    logger = Logger("myTag", listOf(handler1, handler2))
+    logger.log(LogMessage(message = "dummy message"))
+
+    inOrder(handler) {
+      verify(handler).log(any(), argThat { message == "handler1: start handler1#log" })
+      verify(handler).log(any(), argThat { message == "handler2: start handler1#log" })
+      verify(handler).log(any(), argThat { message == "handler1: dummy message" })
+      verify(handler).log(any(), argThat { message == "handler1: end handler1#log" })
+      verify(handler).log(any(), argThat { message == "handler2: end handler1#log" })
+
+      verify(handler).log(any(), argThat { message == "handler1: start handler2#log" })
+      verify(handler).log(any(), argThat { message == "handler2: start handler2#log" })
+      verify(handler).log(any(), argThat { message == "handler2: dummy message" })
+      verify(handler).log(any(), argThat { message == "handler1: end handler2#log" })
+      verify(handler).log(any(), argThat { message == "handler2: end handler2#log" })
+
+      verifyNoMoreInteractions()
+    }
+  }
+
+  @Test
+  fun log_GivenManyLoggersProducingLogsWhileLogging_LogItOnlyOnceAndStopRecursion() {
+    lateinit var handler1: LogHandler
+    lateinit var handler2: LogHandler
+
+    fun newLogger(): Logger = Logger("myTag", listOf(handler1, handler2))
+
+    val handler = mock<LogHandler>()
+
+    handler1 = object : LogHandler {
+      override fun log(tag: String, logMessage: LogMessage) {
+        val logger = newLogger()
+        logger.debug("start handler1#log")
+        handler.log(tag, logMessage.copy(message = "handler1: ${logMessage.message}"))
+        logger.debug("end handler1#log")
+      }
+    }
+
+    handler2 = object : LogHandler {
+      override fun log(tag: String, logMessage: LogMessage) {
+        val logger = newLogger()
+        logger.debug("start handler2#log")
+        handler.log(tag, logMessage.copy(message = "handler2: ${logMessage.message}"))
+        logger.debug("end handler2#log")
+      }
+    }
+
+    newLogger().log(LogMessage(message = "dummy message"))
+
+    inOrder(handler) {
+      verify(handler).log(any(), argThat { message == "handler1: start handler1#log" })
+      verify(handler).log(any(), argThat { message == "handler2: start handler1#log" })
+      verify(handler).log(any(), argThat { message == "handler1: dummy message" })
+      verify(handler).log(any(), argThat { message == "handler1: end handler1#log" })
+      verify(handler).log(any(), argThat { message == "handler2: end handler1#log" })
+
+      verify(handler).log(any(), argThat { message == "handler1: start handler2#log" })
+      verify(handler).log(any(), argThat { message == "handler2: start handler2#log" })
+      verify(handler).log(any(), argThat { message == "handler2: dummy message" })
+      verify(handler).log(any(), argThat { message == "handler1: end handler2#log" })
+      verify(handler).log(any(), argThat { message == "handler2: end handler2#log" })
+
+      verifyNoMoreInteractions()
+    }
   }
 }
