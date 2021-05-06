8ec4cc77e4a75066d827597d008cbaef72679cad
p.turpin <p.turpin@criteo.com>
2021-05-06 11:57:24 +0200

    Bump to version 4.4.0

--- buildSrc/src/main/java/SdkVersion.kt
+++ buildSrc/src/main/java/SdkVersion.kt
@@ -18,7 +18,7 @@ import org.gradle.api.Project
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-private const val sdkBaseVersion = "4.3.0"
+private const val sdkBaseVersion = "4.4.0"
 
 private val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmm"))
 
