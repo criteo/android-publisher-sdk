8ac08d4037c3f4c5eed43097a564c9c9f9e55c69
Viktor Rasevych <v.rasevych@criteo.com>
2023-07-20 19:27:12 +0300

    🔖 Bump to version 5.0.2

--- buildSrc/src/main/java/SdkVersion.kt
+++ buildSrc/src/main/java/SdkVersion.kt
@@ -18,7 +18,7 @@ import org.gradle.api.Project
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-private const val sdkBaseVersion = "5.0.1"
+private const val sdkBaseVersion = "5.0.2"
 
 private val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmm"))
 
